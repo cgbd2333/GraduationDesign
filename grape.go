f282070f3c57fd1c2956998df40b3e625e48a7ac
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-03-16 15:28:38 +0800

    Update grape.go
    改成grapes，完成生产者的初步溯源，有批次，生产者，温度，PH值

--- grape.go
+++ grape.go
@@ -11,26 +11,27 @@ type SmartContract struct{
 	contractapi.Contract
 }
 
-type Asset struct {
-	ID 		string `json:"id"`
-	Owner	string `json:"owner"`
-	Value	int    `json:"value"`
+type Grape struct {
+	Batch 	 	 string		`json:"batch"`		//批次
+	Producer 	 string		`json:"producer"`	//生产者
+	Temperature	 float32	`json:"temperature"`//温度
+	PH		 	 float32	`json:"ph"`			//PH值
 }
 
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	assets := []Asset{
-		{ID:"asset1",Owner:"zhangsan",Value:300},
-		{ID:"asset2",Owner:"LiSi",Value:400},
-		{ID:"asset3",Owner:"Klay",Value:500},
+	grapes := []Grape{
+		{Batch:"20220315-1",Producer:"ZhangSan",Temperature:20.0,PH:6.0},
+		{Batch:"20220315-2",Producer:"LiSi",Temperature:21.2,PH:5.0},
+		{Batch:"20220316-1",Producer:"WangWu",Temperature:22.0,PH:5.8},
 	}
 
-	for _,asset := range assets {
-		assetJSON,err := json.Marshal(asset)
+	for _,grape := range grapes {
+		grapeJSON,err := json.Marshal(grape)
 		if err !=nil {
 			return err
 		}
 
-		err=ctx.GetStub().PutState(asset.ID,assetJSON)
+		err=ctx.GetStub().PutState(grape.Batch,grapeJSON)
 		if err != nil {
 			return fmt.Errorf("failed to put to world state. %v",err)
 		}
@@ -39,80 +40,81 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	return nil
 }
 
-func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface,id string) (*Asset,error) {
-	assetJSON,err := ctx.GetStub().GetState(id)
+func (s *SmartContract) QuaryGrape(ctx contractapi.TransactionContextInterface,batch string) (*Grape,error) {
+	grapeJSON,err := ctx.GetStub().GetState(batch)
 	if err != nil{
 		return nil ,fmt.Errorf("failed to read from world state: %v",err)
 	}
-	if assetJSON == nil {
-		return nil,fmt.Errorf("the asset %s does not exist",id)
+	if grapeJSON == nil {
+		return nil,fmt.Errorf("the grape %s does not exist",batch)
 	}
 
-	var asset Asset
-	err=json.Unmarshal(assetJSON , &asset)
+	var grape Grape
+	err=json.Unmarshal(grapeJSON , &grape)
 	if err != nil{
 		return nil,err
 	}
 
-	return &asset,nil
+	return &grape,nil
 }
 
-func (s *SmartContract) GetALLAssets(ctx contractapi.TransactionContextInterface) ([]*Asset,error){
+func (s *SmartContract) QuaryAllGrapes(ctx contractapi.TransactionContextInterface) ([]*Grape,error){
 	resultsIterator,err := ctx.GetStub().GetStateByRange("","")
 	if err != nil {
 		return nil,err
 	}
 	defer resultsIterator.Close()
 
-	var assets []*Asset
+	var grapes []*Grape
 	for resultsIterator.HasNext(){
 		queryResponse,err:=resultsIterator.Next()
 		if err !=nil{
 			return nil,err
 		}
 
-		var asset Asset
-		err=json.Unmarshal(queryResponse.Value, &asset)
+		var grape Grape
+		err=json.Unmarshal(queryResponse.Value, &grape)
 		if err!=nil{
 			return nil,err
 		}
-		assets=append(assets,&asset)
+		grapes=append(grapes,&grape)
 	}
 
-	return assets,nil
+	return grapes,nil
 }
 
-func (s *SmartContract) AddAsset(ctx contractapi.TransactionContextInterface,id string, owner string, value int) error {
-	asset := Asset{
-			ID: id,
-			Owner: owner,
-			Value: value,
+func (s *SmartContract) AddGrape(ctx contractapi.TransactionContextInterface,batch string, producer string, temperature float32, ph float32) error {
+	grape := Grape{
+			Batch: batch,
+			Producer: producer,
+			Temperature: temperature,
+			PH: ph,
 	}
-	assetJSON,_ := json.Marshal(asset)
+	grapeJSON,_ := json.Marshal(grape)
 
-	return ctx.GetStub().PutState(id,assetJSON)
+	return ctx.GetStub().PutState(batch,grapeJSON)
 }
 
-func (s *SmartContract) ChangeOwner(ctx contractapi.TransactionContextInterface, id string, newOwner string) error {
-	asset,err:= s.ReadAsset(ctx,id)
+func (s *SmartContract) ChangeProducer(ctx contractapi.TransactionContextInterface, batch string, newProducer string) error {
+	grape,err:= s.QuaryGrape(ctx,batch)
 
 	if err != nil {
 		return err
 	}
 
-	asset.Owner=newOwner
+	grape.Producer=newProducer
 
-	assetJSON,_ :=json.Marshal(asset)
-	return ctx.GetStub().PutState(id,assetJSON)
+	grapeJSON,_ :=json.Marshal(grape)
+	return ctx.GetStub().PutState(batch,grapeJSON)
 }
 
 func main(){
-	assetChaincode,err := contractapi.NewChaincode(&SmartContract{})
+	grapeChaincode,err := contractapi.NewChaincode(&SmartContract{})
 	if err != nil{
-		log.Panicf("Error creating asset-transfer-basic chaincode: %v",err)
+		log.Panicf("Error creating grape-transfer-basic chaincode: %v",err)
 	}
 
-	if err:= assetChaincode.Start();err!=nil{
-		log.Panicf("Error starting asser-transfer-basic chaincode: %v",err)
+	if err:= grapeChaincode.Start();err!=nil{
+		log.Panicf("Error starting grape-transfer-basic chaincode: %v",err)
 	}
 }
