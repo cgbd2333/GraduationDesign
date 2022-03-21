030581b52963f326cf32bf6425861f64ff47b9e6
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-03-21 18:52:11 +0800

    Update grape.go
    完成对生产，物流，销售的三方结构体嵌套

--- grape.go
+++ grape.go
@@ -11,34 +11,57 @@ type SmartContract struct{
 	contractapi.Contract
 }
 
-type Tel struct {
-	TEL		string `json:"TEL"`
+type Producer struct {
+	Name		 string 	`json:"Name"`		//种植方姓名
+	Tel			 string 	`json:"Tel"`		//种植方电话
+	Temperature	 float32	`json:"Temperature"`//大棚温度
+	PH		 	 float32	`json:"PH"`			//土壤PH值
+	Time		 string		`json:"Time`		//采摘时间
+}
+
+type Transporter struct {
+	Name		string		`json:"Name"`		//运输司机姓名
+	Tel			string 		`json:"Tel"`		//司机电话
+	CarNum		string		`json:"CarNum"`		//车牌号
+	StartAddress string		`json:"StartAddress"`//出发地
+	StartTime	string		`json:"StartTime"`	//出发时间
+	EndAddress	string		`json:"EndAddress"`//目的地
+	EndTime		string		`json:"EndTime"`	//到达时间
+}
+
+type Sale struct {
+	Name		string		`json:"Name"`		//销售员姓名
+	Time		string		`json:"Time"`		//售卖时间
 }
 
 type Grape struct {
 	Batch 	 	 string		`json:"Batch"`		//批次
-	Producer 	 string		`json:"Producer"`	//生产者
-	Tel			 Tel		`json:"Tel"`
-	Temperature	 float32	`json:"Temperature"`//温度
-	PH		 	 float32	`json:"PH"`			//PH值
+	Producer	 Producer	`json:"Producer"`	//生产
+	Transporter  Transporter `json:"Transporter"`//运输
+	Sale		 Sale		`json:"Sale"`		//售卖
 }
 
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	grapes := []Grape{
-		{Batch: "20220315-1",Producer: "ZhangSan",Tel:Tel{TEL:"123456"},Temperature: 20.0,PH: 6.0},
 		{
-			Batch: "20220315-2",
-			Producer: "LiSi",
-			Tel: Tel{TEL:"123457"},
-			Temperature: 21.2,
-			PH: 5.0,
-		},
-		{
-			Batch: "20220316-1",
-			Producer: "WangWu",
-			Tel: Tel{TEL:"123458"},
-			Temperature: 22.0,
-			PH: 5.8,
+			Batch: "1",
+			Producer: Producer {
+				Name: "ZhangSan",
+				Tel: "123456",
+				Temperature: 22.0,
+				PH: 5.0,
+				Time: "20220321"},
+			Transporter: Transporter {
+				Name: "LiSi",
+				Tel: "321654",
+				CarNum: "2564",
+				StartAddress: "ChangSha,HuNan",
+				StartTime: "20220321",
+				EndAddress: "WuHan,HuBei",
+				EndTime: "20220322"},
+			Sale: Sale {
+				Name: "WangWu",
+				Time: "20220323"},
 		},
 	}
 
@@ -99,7 +122,7 @@ func (s *SmartContract) QuaryAllGrapes(ctx contractapi.TransactionContextInterfa
 
 	return grapes,nil
 }
-
+/*
 func (s *SmartContract) AddGrape(ctx contractapi.TransactionContextInterface,batch string, producer string, temperature float32, ph float32) error {
 	grape := Grape{
 			batch,
@@ -112,7 +135,8 @@ func (s *SmartContract) AddGrape(ctx contractapi.TransactionContextInterface,bat
 
 	return ctx.GetStub().PutState(batch,grapeJSON)
 }
-
+*/
+/*
 func (s *SmartContract) ChangeProducer(ctx contractapi.TransactionContextInterface, batch string, newProducer string) error {
 	grape,err:= s.QuaryGrape(ctx,batch)
 
@@ -125,7 +149,8 @@ func (s *SmartContract) ChangeProducer(ctx contractapi.TransactionContextInterfa
 	grapeJSON,_ :=json.Marshal(grape)
 	return ctx.GetStub().PutState(batch,grapeJSON)
 }
-
+*/
+/*
 func (s *SmartContract) ChangeTel(ctx contractapi.TransactionContextInterface, batch string, newTel string) error {
 	grape,err:= s.QuaryGrape(ctx,batch)
 
@@ -138,7 +163,7 @@ func (s *SmartContract) ChangeTel(ctx contractapi.TransactionContextInterface, b
 	grapeJSON,_ :=json.Marshal(grape)
 	return ctx.GetStub().PutState(batch,grapeJSON)
 }
-
+*/
 func main(){
 	grapeChaincode,err := contractapi.NewChaincode(&SmartContract{})
 	if err != nil{
