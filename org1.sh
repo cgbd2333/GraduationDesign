030c97a4d84af4ded1b5ba97a0f1cc6fa6572b06
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-03-23 10:35:23 +0800

    Update org1.sh
    脚本命令换行，增强可读性

--- org1.sh
+++ org1.sh
@@ -5,15 +5,23 @@ export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=${PWD}/../config/
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrg\
+anizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrg\
+anizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
 
 ./network.sh up createChannel
+echo "链码已启动"
 
-peer lifecycle chaincode package grape.tar.gz --path /home/hong/grape --lang golang --label grape_1
+peer lifecycle chaincode package grape.tar.gz \
+--path /home/hong/grape `# grape.go所在的文件路径`\
+--lang golang \
+--label grape_1
+echo "打包已完成"
 
 peer lifecycle chaincode install grape.tar.gz &> id.txt
+echo "安装已完成"
 
 cat grapeid.txt
 
@@ -23,9 +31,27 @@ id3=""grape_1:"$id2"
 
 
 
-cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/org\
+anizations/ordererOrganizations/example.com/orderers/orderer.exam\
+ple.com/msp/tlscacerts/tlsca.example.com-cert.pem
 grape_id=$id3
 
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $cafile  --channelID mychannel --name grape --version 1.0 --package-id $grape_id Label: grape_1 --sequence 1
+peer lifecycle chaincode approveformyorg -o localhost:7050 \
+--ordererTLSHostnameOverride orderer.example.com \
+--tls \
+--cafile $cafile \
+--channelID mychannel \
+--name grape \
+--version 1.0 \
+--package-id $grape_id Label: grape_1 \
+--sequence 1
 
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name grape --version 1.0 --sequence 1 --output json
+echo "背书已完成"
+
+peer lifecycle chaincode checkcommitreadiness \
+--channelID mychannel \
+--name grape \
+--version 1.0 \
+--sequence 1 \
+--output json
+
