24d426d441009e4911ba1142fef53ac56155430e
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-04-01 20:23:04 +0800

    Update commit.sh
    完善脚本，当某个命令运行出错时直接退出整个脚本

--- commit.sh
+++ commit.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
+
 PWD='/opt/fabric-2.3.0/scripts/fabric-samples/test-network'
 export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
 
 cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/org\
 anizations/ordererOrganizations/example.com/orderers/orderer.exam\
@@ -28,7 +36,12 @@ peer lifecycle chaincode commit  \
 --tlsRootCertFiles $org2_CertFiles \
 --version 1.0 \
 --sequence 1
-echo "链码已提交"
+if [ $? -eq 0 ]; then
+     echo "链码提交成功"
+else
+     echo "链码提交失败"
+	   exit 1
+fi
 
 peer chaincode invoke \
 -o orderer.example.com:7050 \
@@ -41,4 +54,9 @@ peer chaincode invoke \
 --peerAddresses peer0.org2.example.com:9051 \
 --tlsRootCertFiles $org2_CertFiles \
 -c '{"Args":["InitLedger"]}'
-echo "链码已初始化"
+if [ $? -eq 0 ]; then
+     echo "链码初始化成功"
+else
+     echo "链码初始化失败"
+	   exit 1
+fi
