6eceacdf1348f052c76805edd40d2bc8e3109330
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-04-15 16:07:35 +0800

    Update TransporterInsert.sh
    增加权限控制，只有7051端口能调用函数

--- TransporterInsert.sh
+++ TransporterInsert.sh
@@ -7,16 +7,20 @@ export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrg\
 anizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrg\
-anizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=localhost:7051
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrgan\
+izations/org1.example.com/users/Admin@org1.example.com/msp
 
-cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/org\
-anizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-org1_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/org\
-anizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt 
-org2_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/org\
-anizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/orga\
+nizations/ordererOrganizations/example.com/orderers/orderer.examp\
+le.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+org1_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-netwo\
+rk/organizations/peerOrganizations/org1.example.com/peers/peer0.or\
+g1.example.com/tls/ca.crt 
+
+org2_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-netw\
+ork/organizations/peerOrganizations/org2.example.com/peers/peer0.o\
+rg2.example.com/tls/ca.crt
 
 Insert() {
 	arg1=$1
@@ -37,23 +41,28 @@ Insert() {
 	-n --arg val6 "$arg6" \
 	-n --arg val7 "$arg7" \
 	-n --arg val8 "$arg8" \
-'{"Args":["AddTranspoter",$val1,$val2,$val3,$val4,$val5,$val6,$val7,$val8]}'
+	'{"Args":["AddTranspoter",$val1,$val2,$val3,$val4,$val5,$val6,$val7,$val8]}'
 	)
 
 	#echo $args
 
+	if [ ${CORE_PEER_ADDRESS} == "localhost:7051" ] 
+	then
+		peer chaincode invoke \
+		-o orderer.example.com:7050 \
+		--tls true \
+		--cafile $cafile \
+		-C mychannel \
+		-n grape \
+		--peerAddresses peer0.org1.example.com:7051 \
+		--tlsRootCertFiles $org1_CertFiles \
+		--peerAddresses peer0.org2.example.com:9051 \
+		--tlsRootCertFiles $org2_CertFiles \
+		-c "$args"
+	else
+		echo "您没有足够的权限"
+	fi
 
-	peer chaincode invoke \
-    -o orderer.example.com:7050 \
-    --tls true \
-    --cafile $cafile \
-    -C mychannel \
-    -n grape \
-    --peerAddresses peer0.org1.example.com:7051 \
-    --tlsRootCertFiles $org1_CertFiles \
-    --peerAddresses peer0.org2.example.com:9051 \
-    --tlsRootCertFiles $org2_CertFiles \
-    -c "$args"
 }
 
 Insert "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8"
