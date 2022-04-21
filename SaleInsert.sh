cc3eeb32c95e69045c16e0cee71e15eb608f5dd9
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-04-21 16:55:38 +0800

    Update SaleInsert.sh
    适配org3

--- SaleInsert.sh
+++ SaleInsert.sh
@@ -4,23 +4,27 @@ PWD='/opt/fabric-2.3.0/scripts/fabric-samples/test-network'
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=${PWD}/../config/
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrg\
-anizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrgan\
-izations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peer\
+Organizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrga\
+nizations/org3.example.com/users/Admin@org3.example.com/msp
 
-cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/or\
-ganizations/ordererOrganizations/example.com/orderers/orderer.ex\
-ample.com/msp/tlscacerts/tlsca.example.com-cert.pem
+cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/org\
+anizations/ordererOrganizations/example.com/orderers/orderer.exam\
+ple.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 org1_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-networ\
-k/organizations/peerOrganizations/org1.example.com/peers/peer0.o\
-rg1.example.com/tls/ca.crt 
+k/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.e\
+xample.com/tls/ca.crt 
 
-org2_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-net\
-work/organizations/peerOrganizations/org2.example.com/peers/peer\
-0.org2.example.com/tls/ca.crt
+org2_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/o\
+rganizations/peerOrganizations/org2.example.com/peers/peer0.org2.exam\
+ple.com/tls/ca.crt
+
+org3_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-netw\
+ork/organizations/peerOrganizations/org3.example.com/peers/peer0.or\
+g3.example.com/tls/ca.crt
 
 Insert() {
 	arg1=$1
@@ -37,7 +41,7 @@ Insert() {
 
 	#echo $args
 
-    if [ ${CORE_PEER_ADDRESS} == "localhost:7051" ] 
+    if [ ${CORE_PEER_ADDRESS} == "localhost:11051" ] 
 	then
 		peer chaincode invoke \
         -o orderer.example.com:7050 \
@@ -49,11 +53,13 @@ Insert() {
         --tlsRootCertFiles $org1_CertFiles \
         --peerAddresses peer0.org2.example.com:9051 \
         --tlsRootCertFiles $org2_CertFiles \
+        --peerAddresses peer0.org3.example.com:11051 \
+        --tlsRootCertFiles $org3_CertFiles \
         -c "$args"
 	else
 		echo "您没有足够的权限"
 	fi
-
+    
 }
 
 Insert "$1" "$2" "$3"
