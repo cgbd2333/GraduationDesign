3f3e26d589f04b4bef9fcde6184603fde9335d7d
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-04-21 16:44:14 +0800

    Create org3_queryall.sh
    org3全部查询

---
+++ org3_queryall.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+PWD='/opt/fabric-2.3.0/scripts/fabric-samples/test-network'
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+
+peer chaincode query -C mychannel -n grape -c '{"Args":["QuaryAllGrapes"]}' #>out.json
+
+#cd /home/hong/qrcode
+
+#go run qrcode.go
