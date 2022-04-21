0b9830e515a450082711c46111ae18dff05d6f61
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-04-21 16:57:07 +0800

    Update TransporterInsert.sh
    增加org3

--- TransporterInsert.sh
+++ TransporterInsert.sh
@@ -4,11 +4,11 @@ PWD='/opt/fabric-2.3.0/scripts/fabric-samples/test-network'
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=${PWD}/../config/
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_LOCALMSPID="Org2MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrg\
-anizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+anizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrgan\
-izations/org1.example.com/users/Admin@org1.example.com/msp
+izations/org2.example.com/users/Admin@org2.example.com/msp
 
 cafile=/opt/fabric-2.3.0/scripts/fabric-samples/test-network/orga\
 nizations/ordererOrganizations/example.com/orderers/orderer.examp\
@@ -21,6 +21,10 @@ g1.example.com/tls/ca.crt
 org2_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-netw\
 ork/organizations/peerOrganizations/org2.example.com/peers/peer0.o\
 rg2.example.com/tls/ca.crt
+
+org3_CertFiles=/opt/fabric-2.3.0/scripts/fabric-samples/test-netw\
+ork/organizations/peerOrganizations/org3.example.com/peers/peer0.o\
+rg3.example.com/tls/ca.crt
 
 Insert() {
 	arg1=$1
@@ -46,7 +50,7 @@ Insert() {
 
 	#echo $args
 
-	if [ ${CORE_PEER_ADDRESS} == "localhost:7051" ] 
+	if [ ${CORE_PEER_ADDRESS} == "localhost:9051" ] 
 	then
 		peer chaincode invoke \
 		-o orderer.example.com:7050 \
@@ -58,6 +62,8 @@ Insert() {
 		--tlsRootCertFiles $org1_CertFiles \
 		--peerAddresses peer0.org2.example.com:9051 \
 		--tlsRootCertFiles $org2_CertFiles \
+		--peerAddresses peer0.org3.example.com:11051 \
+		--tlsRootCertFiles $org3_CertFiles \
 		-c "$args"
 	else
 		echo "您没有足够的权限"
