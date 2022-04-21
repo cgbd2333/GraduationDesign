d1922b15b2a63e853f4cf488c7ce471e747a0c30
cgbd2333 <71622533+cgbd2333@users.noreply.github.com>
2022-04-21 16:47:22 +0800

    Create org2_query.sh
    org2查询

---
+++ org2_query.sh
@@ -0,0 +1,35 @@
+ #! /bin/bash
+
+PWD='/opt/fabric-2.3.0/scripts/fabric-samples/test-network'
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrg\
+anizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrg\
+anizations/org2.example.com/users/Admin@org2.example.com/msp
+#export CORE_PEER_ADDRESS=localhost:9051
+
+query() {
+	str=$1
+
+	args=$(jq \
+	-n --arg val "$str" \
+'{"Args":["QuaryGrape",$val]}'
+	)
+
+	#echo $args
+	peer chaincode query -C mychannel -n grape -c "$args"
+
+	
+	
+}
+
+query "$1"
+
+#query "$1" >out.json
+
+#cd /home/hong/qrcode
+
+#go run qrcode.go
